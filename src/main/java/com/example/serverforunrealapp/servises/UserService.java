ff3de3ca32d812a4c1351b4d565d0afe0025918b
kirill1881 <82600534+kirill1881@users.noreply.github.com>
2022-11-06 15:57:39 +0300

    123

--- src/main/java/com/example/serverforunrealapp/servises/UserService.java
+++ src/main/java/com/example/serverforunrealapp/servises/UserService.java
@@ -1,5 +1,6 @@
 package com.example.serverforunrealapp.servises;
 
+import com.example.serverforunrealapp.models.UserModel;
 import com.example.serverforunrealapp.repos.UserRepo;
 import org.springframework.stereotype.Service;
 
@@ -12,6 +13,13 @@ public class UserService {
     }
 
     public String login(String login, String password){
-
+        UserModel userModel = null;
+        userModel = userRepo.findUserModelByLogin(login);
+        if (userModel==null){
+            return "no user";
+        }else if (userModel.getPassword().equals(password)){
+            return userModel.toString();
+        }
+        return "not correct password";
     }
 }
