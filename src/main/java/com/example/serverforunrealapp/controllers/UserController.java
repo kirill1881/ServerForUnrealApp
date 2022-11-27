9435cc08f1be9546585f26106fb50e12fd247f79
kirill1881 <82600534+kirill1881@users.noreply.github.com>
2022-11-27 15:31:46 +0300

    123

--- src/main/java/com/example/serverforunrealapp/controllers/UserController.java
+++ src/main/java/com/example/serverforunrealapp/controllers/UserController.java
@@ -37,15 +37,15 @@ public class UserController {
         return userRepo.findAll().toString();
     }
 
-    @PostMapping
+    @PostMapping("/editName")
     public boolean editName( @RequestParam long id, @RequestParam String newName) {
         return userService.editName(id, newName);
     }
-    @PostMapping
+    @PostMapping("/editLastName")
     public boolean editLastName( @RequestParam long id, @RequestParam String newLastName) {
         return userService.editLastName(id, newLastName);
     }
-    @PostMapping
+    @PostMapping("/editUrl")
     public boolean editUrl( @RequestParam long id, @RequestParam String url) {
         return userService.editUrl(id, url);
     }
